43db0689e1310dd82146c1ca63f021024d57176f
IvanMenshykov <menshikov.ivn@gmail.com>
2017-02-19 11:28:16 +0100

    add some function

--- box.go
+++ box.go
@@ -26,7 +26,19 @@ type EdgeSizes struct {
 type LayoutBox struct {
 	dimensions Dimensions
 	box_type   interface{} //box_type can be a block node, an inline node, or an anonymous block box
-	childre    map[int]LayoutBox
+	children   map[int]LayoutBox
+}
+
+func (l LayoutBox) getStyleNode() interface{} {
+	boxType := l.box_type
+	switch style := boxType.(type) {
+	case BlockNode, InlineNode:
+		return style
+	case AnonymousBlock:
+		panic("Anonymous block box has no style node")
+	default:
+		panic("Type must be BlockNode or InlineNode")
+	}
 }
 
 type BlockNode struct {
@@ -59,7 +71,9 @@ func (s StyleNode) display() string {
 
 func NewLayoutBox(boxType interface{}) LayoutBox {
 	return LayoutBox{
-		box_type: boxType,
+		dimensions: Dimensions{},
+		box_type:   boxType,
+		children:   map[int]LayoutBox{},
 	}
 }
 
@@ -75,5 +89,71 @@ func buildLayoutTree(styleNode StyleNode) LayoutBox {
 		panic("Root node has display: none.")
 	}
 	root := NewLayoutBox(boxType)
+
+	for _, child := range styleNode.children {
+		display = child.display()
+		switch display {
+		case "block":
+			root.children[len(root.children)] = buildLayoutTree(child)
+		case "inline":
+			inline := root.getInlineContainer()
+			inline.children[len(inline.children)] = buildLayoutTree(child)
+		default:
+		}
+	}
+
 	return root
+}
+
+func (l LayoutBox) getInlineContainer() LayoutBox {
+	boxT := l.box_type
+	switch boxT.(type) {
+	case AnonymousBlock:
+		return l
+	case BlockNode:
+		return NewLayoutBox(BlockNode{})
+		//switch childBoxType := value.(l.children[len(l.children)-1].box_type) {
+		//}
+	default:
+		return l
+	}
+}
+
+func (l LayoutBox) layout(containBlock Dimensions) {
+	switch l.box_type.(type) {
+	case BlockNode:
+		l.layoutBox(containBlock)
+	case InlineNode:
+		//TODO
+	case AnonymousBlock:
+		//TODO
+	}
+}
+
+func (l LayoutBox) layoutBox(containBlock Dimensions) {
+	//Child width can depend on parent width, so we need to calculate
+	//this box's width before laying out its children.
+	l.calculateBlockWidth(containBlock)
+
+	//Determine where the box is located within its container.
+	l.calculateBlockPosition(containBlock)
+
+	//Parent height can depend on child height, so calculateHeight
+	//must be called after the children are laid out.
+	l.calculateBlockHeight(containBlock)
+}
+
+func (l LayoutBox) calculateBlockWidth(containBlock Dimensions) {
+	//style := l.getStyleNode()
+
+	//width has initial value auto
+
+	//margin, border, and padding have initial value 0
+	//zero := Length{0.0, "px"}
+}
+
+func (l LayoutBox) calculateBlockPosition(containBlock Dimensions) {
+}
+
+func (l LayoutBox) calculateBlockHeight(containBlock Dimensions) {
 }
